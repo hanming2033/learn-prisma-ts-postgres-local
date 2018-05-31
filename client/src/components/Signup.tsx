991a5063f270e928dc03813dd18d9d5cca70064b
Zheng Hanming <hanming2033@gmail.com>
2018-05-31 21:42:43 +0800

    testing mutation error

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -45,39 +45,50 @@ class Signup extends React.Component<ISignupProps, ISignupState> {
             <WithSignUpMutation mutation={SIGNUP_USER}>
               {(signup, mtnRes) => {
                 return (
-                  <form
-                    onSubmit={async (e: React.SyntheticEvent<HTMLFormElement>) => {
-                      e.preventDefault()
-                      // guarding the submit function so that users wont submit multiple times
-                      if (this.state.isSubmitting) return
-                      // setting the state to true so only first submit will trigger signup mutation
-                      this.setState({ isSubmitting: true })
-                      const response = signup({ variables: { email, name, password } })
-                      // todo:https://www.youtube.com/watch?v=4Y9chGxAqmc
-                      console.log(response.then())
-                      this.setState({ isSubmitting: false })
-                      if (!qryRes.data) return
-                      qryRes.client.writeData({ data: { ...qryRes.data, forms: { ...qryRes.data.forms, input_Signup_Password: '' } } })
-                    }}
-                  >
-                    <input type="text" value={name} onChange={e => handleInputChange(e, 'input_Signup_Name', qryRes)} placeholder="Name" />
-                    <br />
-                    <input
-                      type="text"
-                      value={email}
-                      onChange={e => handleInputChange(e, 'input_Signup_Email', qryRes)}
-                      placeholder="Email"
-                    />
-                    <br />
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={e => handleInputChange(e, 'input_Signup_Password', qryRes)}
-                      placeholder="password"
-                    />
-                    <br />
-                    <button type="submit">Submit</button>
-                  </form>
+                  <>
+                    <form
+                      onSubmit={async (e: React.SyntheticEvent<HTMLFormElement>) => {
+                        e.preventDefault()
+                        // guarding the submit function so that users wont submit multiple times
+                        if (this.state.isSubmitting) return
+                        // setting the state to true so only first submit will trigger signup mutation
+                        this.setState({ isSubmitting: true })
+                        console.log('before signup')
+
+                        await signup({ variables: { email, name, password } })
+                        console.log(mtnRes)
+
+                        this.setState({ isSubmitting: false })
+                        if (!qryRes.data) return
+                        qryRes.client.writeData({ data: { ...qryRes.data, forms: { ...qryRes.data.forms, input_Signup_Password: '' } } })
+                      }}
+                    >
+                      <input
+                        type="text"
+                        value={name}
+                        onChange={e => handleInputChange(e, 'input_Signup_Name', qryRes)}
+                        placeholder="Name"
+                      />
+                      <br />
+                      <input
+                        type="text"
+                        value={email}
+                        onChange={e => handleInputChange(e, 'input_Signup_Email', qryRes)}
+                        placeholder="Email"
+                      />
+                      <br />
+                      <input
+                        type="password"
+                        value={password}
+                        onChange={e => handleInputChange(e, 'input_Signup_Password', qryRes)}
+                        placeholder="password"
+                      />
+                      <br />
+                      <button type="submit">Submit</button>
+                    </form>
+                    {mtnRes.loading && <p>Loading...</p>}
+                    {mtnRes.error && <p>Error Please try again {mtnRes.error.message}</p>}
+                  </>
                 )
               }}
             </WithSignUpMutation>
