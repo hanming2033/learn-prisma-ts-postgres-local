4cdc082de9cf6d1178320d9708fe799c2b7d3fd9
Zheng Hanming <hanming2033@gmail.com>
2018-06-03 11:00:40 +0800

    removed posts related

--- server/src/resolvers/Mutation/post.ts
+++ server/src/resolvers/Mutation/post.ts
@@ -1,55 +1,51 @@
-import { getUserId, IResolverMap } from '../../utils'
-import { CreateDraftMutationArgs, PublishMutationArgs, DeletePostMutationArgs } from '../../generated/server-types'
-import { Post } from '../../generated/prisma'
+import { IResolverMap } from '../../utils'
+// import { CreateDraftMutationArgs, PublishMutationArgs, DeletePostMutationArgs } from '../../generated/server-types'
+// import { Post } from '../../generated/prisma'
 
 export const post: IResolverMap = {
-  async createDraft(_, { title, text }: CreateDraftMutationArgs, ctx, info): Promise<Post> {
-    const userId = getUserId(ctx)
-    return ctx.db.mutation.createPost(
-      {
-        data: {
-          title,
-          text,
-          isPublished: false,
-          author: {
-            connect: { id: userId }
-          }
-        }
-      },
-      info
-    )
-  },
-
-  async publish(_, { id }: PublishMutationArgs, ctx, info): Promise<Post | null> {
-    const userId = getUserId(ctx)
-    const postExists = await ctx.db.exists.Post({
-      id,
-      author: { id: userId }
-    })
-    if (!postExists) {
-      throw new Error(`Post not found or you're not the author`)
-    }
-
-    return ctx.db.mutation.updatePost(
-      {
-        where: { id },
-        data: { isPublished: true }
-      },
-      info
-    )
-  },
-
-  async deletePost(_, { id }: DeletePostMutationArgs, ctx, info): Promise<Post | null> {
-    console.log(info)
-    const userId = getUserId(ctx)
-    const postExists = await ctx.db.exists.Post({
-      id,
-      author: { id: userId }
-    })
-    if (!postExists) {
-      throw new Error(`Post not found or you're not the author`)
-    }
-
-    return ctx.db.mutation.deletePost({ where: { id } })
-  }
+  // async createDraft(_, { title, text }: CreateDraftMutationArgs, ctx, info): Promise<Post> {
+  //   const userId = getUserId(ctx)
+  //   return ctx.db.mutation.createPost(
+  //     {
+  //       data: {
+  //         title,
+  //         text,
+  //         isPublished: false,
+  //         author: {
+  //           connect: { id: userId }
+  //         }
+  //       }
+  //     },
+  //     info
+  //   )
+  // },
+  // async publish(_, { id }: PublishMutationArgs, ctx, info): Promise<Post | null> {
+  //   const userId = getUserId(ctx)
+  //   const postExists = await ctx.db.exists.Post({
+  //     id,
+  //     author: { id: userId }
+  //   })
+  //   if (!postExists) {
+  //     throw new Error(`Post not found or you're not the author`)
+  //   }
+  //   return ctx.db.mutation.updatePost(
+  //     {
+  //       where: { id },
+  //       data: { isPublished: true }
+  //     },
+  //     info
+  //   )
+  // },
+  // async deletePost(_, { id }: DeletePostMutationArgs, ctx, info): Promise<Post | null> {
+  //   console.log(info)
+  //   const userId = getUserId(ctx)
+  //   const postExists = await ctx.db.exists.Post({
+  //     id,
+  //     author: { id: userId }
+  //   })
+  //   if (!postExists) {
+  //     throw new Error(`Post not found or you're not the author`)
+  //   }
+  //   return ctx.db.mutation.deletePost({ where: { id } })
+  // }
 }
--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,27 +1,27 @@
 import { getUserId, IResolverMap } from '../utils'
-import { PostQueryArgs } from '../generated/server-types'
-import { Post, User } from '../generated/prisma'
+// import { PostQueryArgs } from '../generated/server-types'
+import { User } from '../generated/prisma'
 
 export const Query: IResolverMap = {
-  feed(_, __, ctx, info): Promise<Post[]> {
-    return ctx.db.query.posts({ where: { isPublished: true } }, info)
-  },
+  // feed(_, __, ctx, info): Promise<Post[]> {
+  //   return ctx.db.query.posts({ where: { isPublished: true } }, info)
+  // },
 
-  drafts(_, __, ctx, info): Promise<Post[]> {
-    const id = getUserId(ctx)
+  // drafts(_, __, ctx, info): Promise<Post[]> {
+  //   const id = getUserId(ctx)
 
-    const where = {
-      isPublished: false,
-      author: {
-        id
-      }
-    }
-    return ctx.db.query.posts({ where }, info)
-  },
+  //   const where = {
+  //     isPublished: false,
+  //     author: {
+  //       id
+  //     }
+  //   }
+  //   return ctx.db.query.posts({ where }, info)
+  // },
 
-  post(_, args: PostQueryArgs, ctx, info): Promise<Post> {
-    return ctx.db.query.post({ where: { id: args.id } }, info)
-  },
+  // post(_, args: PostQueryArgs, ctx, info): Promise<Post> {
+  //   return ctx.db.query.post({ where: { id: args.id } }, info)
+  // },
 
   me(_, __, ctx, info): Promise<User> {
     const id = getUserId(ctx)
